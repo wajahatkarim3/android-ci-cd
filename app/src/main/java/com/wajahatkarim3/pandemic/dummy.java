1ff368820a3f17fb5aac32cd6490d28fd3cc55cb
Wajahat Karim <wajahatkarim3@gmail.com>
2020-04-01 23:57:57 +0500

    1.0.1

---
+++ app/src/main/java/com/wajahatkarim3/pandemic/dummy.java
@@ -0,0 +1,20 @@
+package com.wajahatkarim3.pandemic;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
+
+class dummy extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        TextView txtUsername = findViewById(R.id.hello);
+
+
+    }
+}
